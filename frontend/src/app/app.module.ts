aec67113ec9b16d368dc499aa5fe99c2d5362c9e
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2023-06-11 19:54:42 +0700

    create routing router for angular url

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { TopUpComponent } from './top-up/top-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    TopUpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      {path: 'auth', component: AuthComponent},
+      {path: 'top-up', component: TopUpComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
