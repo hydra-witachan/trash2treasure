09e5656e3fa2374322ddce8ec298a984ab124f2b
Alviannn <alvianndq@gmail.com>
2023-06-11 20:11:40 +0700

    feat(migration): add create user table

---
+++ backend/migrations/20230611085709_create_users.sql
@@ -0,0 +1,22 @@
+-- migrate:up
+CREATE TABLE users (
+    id VARCHAR(36) NOT NULL DEFAULT UUID(),
+
+    username VARCHAR(64) NOT NULL,
+    full_name VARCHAR(256) NOT NULL,
+    email VARCHAR(128) NOT NULL,
+    address VARCHAR(512) NOT NULL,
+    password VARCHAR(128) NOT NULL,
+    role VARCHAR() NOT NULL,
+
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    deleted_at TIMESTAMP,
+
+    PRIMARY KEY (id),
+    INDEX (deleted_at),
+    CHECK (role IN ('donator', 'collector'))
+);
+
+-- migrate:down
+DROP TABLE users;
