a407ee6d2adf1fecc9e05f27a6ac434791579a16
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2023-06-12 14:33:10 +0700

    create confirmation top up

--- frontend/src/app/confirmation-top-up/confirmation-top-up.component.ts
+++ frontend/src/app/confirmation-top-up/confirmation-top-up.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MySharedService } from '../shared/my-shared-service.service';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-confirmation-top-up',
@@ -10,12 +11,21 @@ export class ConfirmationTopUpComponent {
   topUpPoint: any;
   formattedPoint: any;
   feeTopUp: any;
+  formattedFeeTopUp: any;
 
-  constructor(private sharedService: MySharedService) {}
+  constructor(private sharedService: MySharedService, private decimalPipe: DecimalPipe) {}
 
   ngOnInit() {
     this.topUpPoint = this.sharedService.getPointTopUp();
     this.formattedPoint = this.sharedService.getPointTopUpFormated();
     this.feeTopUp = this.sharedService.getFeePointTopUp();
+    this.formattedFeeTopUp = this.decimalPipe.transform(this.feeTopUp, '1.0-3');
+    if(this.formattedFeeTopUp !== null) {
+      this.formattedFeeTopUp =  this.formattedFeeTopUp.replaceAll(',', '.')
+    }
+  }
+
+  submit() {
+    console.log("HELLO");
   }
 }
