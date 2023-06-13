0d17465e6c0fd1a2d1393b6d4c9d7a21e7fab5c1
Alviannn <alvianndq@gmail.com>
2023-06-13 21:21:00 +0700

    refactor(model): reformat code

--- backend/internal/models/items_model.go
+++ backend/internal/models/items_model.go
@@ -3,12 +3,12 @@ package models
 type Item struct {
 	BaseModel
 
-	AuthorID        string          `gorm:"column:author_id" json:"authorId"`
-	AuthorName      string          `gorm:"column:author_name" json:"authorName"`
-	ItemName        string          `gorm:"column:item_name" json:"itemName"`
-    Description     string          `gorm:"column:description" json:"description"`
-    Points          int             `gorm:"column:points" json:"points"`
-    ImageURL        string          `gorm:"column:image_url" json:"imageUrl"`
-    NeededAmount    int             `gorm:"column:needed_amount" json:"neededAmount"`
-    FullfiledAmount int             `gorm:"column:fullfiled_amount" json:"fullfiledAmount"`
+	AuthorID        string `gorm:"column:author_id" json:"authorId"`
+	AuthorName      string `gorm:"column:author_name" json:"authorName"`
+	ItemName        string `gorm:"column:item_name" json:"itemName"`
+	Description     string `gorm:"column:description" json:"description"`
+	Points          int    `gorm:"column:points" json:"points"`
+	ImageURL        string `gorm:"column:image_url" json:"imageUrl"`
+	NeededAmount    int    `gorm:"column:needed_amount" json:"neededAmount"`
+	FullfiledAmount int    `gorm:"column:fullfiled_amount" json:"fullfiledAmount"`
 }
