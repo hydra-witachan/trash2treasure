3807c563cf20221ba976f3761e243464e0ee2a5c
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2023-06-15 11:06:01 +0700

    submit button routing

--- frontend/src/app/upload-item/upload-item.component.ts
+++ frontend/src/app/upload-item/upload-item.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-upload-item',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class UploadItemComponent {
 
+  constructor(private router: Router) {
+
+  }
+
+
+  submit() {
+    this.router.navigate(['/home']);
+  }
 }
