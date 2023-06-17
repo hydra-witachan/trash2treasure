84b7842714eb318a9bcc6fde2f5a4bee694ee684
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2023-06-18 02:23:48 +0700

    fix: remove unncesecarry logic

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,10 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  ngOnInit() {
-    const role = localStorage.getItem("role");
-    if(!role) {
-      localStorage.setItem("role", "guest");
-    }
-  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import routes from './routes';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { DecimalPipe } from '@angular/common';
@@ -5,7 +6,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { TopUpComponent } from './top-up/top-up.component';
-import routes from './routes';
 import { FooterComponent } from './footer/footer.component';
 import { ConfirmationTopUpComponent } from './confirmation-top-up/confirmation-top-up.component';
 import { TransactionSuccessComponent } from './transaction-success/transaction-success.component';
@@ -21,6 +21,7 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RedeemPointsComponent } from './redeem-points/redeem-points.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -40,6 +41,7 @@ import { RedeemPointsComponent } from './redeem-points/redeem-points.component';
     SignInComponent,
     SignUpComponent,
     RedeemPointsComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
