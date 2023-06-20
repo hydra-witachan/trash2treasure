bff23758bd68a42e014819522ea371333ea5470f
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2023-06-20 16:53:57 +0700

    feat: fetching get item collector

--- frontend/src/app/home-collector/home-collector.component.ts
+++ frontend/src/app/home-collector/home-collector.component.ts
@@ -1,13 +1,47 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'app-home-collector',
   templateUrl: './home-collector.component.html',
-  styleUrls: ['./home-collector.component.css']
+  styleUrls: ['./home-collector.component.css'],
 })
 export class HomeCollectorComponent {
-  constructor(private router: Router) {
+  items: any[] = [];
+  accessToken: string = '';
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private router: Router
+  ) {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      this.accessToken = token;
+    }
+  }
+
+  ngOnInit() {
+    const token = this.accessToken;
+    if (token) {
+      const decode: any = jwt_decode(token);
+      const id = decode.id;
+      const url = `http://localhost:5000/items/collectors/${id}`;
+      const headers = new HttpHeaders({
+        Authorization: `Bearer ${token}`,
+      });
+      
+      this.http.get(url, { headers }).subscribe(
+        (response: any) => {
+          this.items = response;
+          console.log(this.items);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    }
   }
 
   upload() {
