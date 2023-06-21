5d0b2626c5d2d655ca609b188d232aace6f1d26f
Alviannn <alvianndq@gmail.com>
2023-06-21 22:56:02 +0700

    feat: share sub category

--- frontend/src/app/list-item/list-item.component.ts
+++ frontend/src/app/list-item/list-item.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MySharedService } from '../shared/my-shared-service.service';
 
 @Component({
   selector: 'app-list-item',
@@ -8,19 +9,19 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./list-item.component.css']
 })
 export class ListItemComponent {
-  subCategory = "Skincare";
+  title = "";
   items: any[] = [];
 
-  constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router) { }
+  constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router, public sharedService: MySharedService) { }
 
   ngOnInit() {
     const subCategory = this.route.snapshot.paramMap.get('subCategory');
+    this.title = this.sharedService.getSubCategory();
     console.log(subCategory); // Do whatever you need with the subCategory value
     const url = `http://localhost:5000/items?sub_category=${subCategory}&search=`;
     console.log(url); // Use the URL for your API request
     this.http.get(url).subscribe((response: any) => {
       this.items = response;
-      console.log(this.items);
     }, (error: any) => console.log(error));
   }
   displayItemDetails(item: any) {
